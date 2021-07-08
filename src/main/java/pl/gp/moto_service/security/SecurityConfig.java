5f527ac47235f7ecfc84855c9fc86741867e2ee7
Grzegorz Przebinda <grzegorz.przebinda@gmail.com>
2021-07-08 13:18:03 +0200

    simple authorized request by role

--- src/main/java/pl/gp/moto_service/security/SecurityConfig.java
+++ src/main/java/pl/gp/moto_service/security/SecurityConfig.java
@@ -2,10 +2,22 @@ package pl.gp.moto_service.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/panel/vehicleslist/**")
+                .hasRole("USER")
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -14,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("ADMIN")
                 .and()
                 .withUser("user")
-                .password("{bcrypt}$2y$06$5/kGNogT0kBnT0tmLbk10.wpo46fegszSU6oqDpcGDyBPVWgabSYm ")
+                .password("{bcrypt}$2y$06$5/kGNogT0kBnT0tmLbk10.wpo46fegszSU6oqDpcGDyBPVWgabSYm")
                 .roles("USER");
     }
 }
